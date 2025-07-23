538b0adacd3cf27d1e582828a3ea053a189e3606
jiazhen427 <25061599@imail.sunway.edu.my>
2025-07-23 03:09:01 +0000

    cconcept

--- cconcept.py
+++ cconcept.py
@@ -1,32 +1,32 @@
 import streamlit as st
 import math
 
-# Expanded car database with images, HP, CC and many brands
+# Car database without images
 cars = [
-    {"name": "Perodua Alza", "seats": 7, "type": "Family", "price": 70000, "image": "https://i.imgur.com/OaV9sQE.jpg", "hp": 105, "cc": 1500},
-    {"name": "Proton Exora", "seats": 7, "type": "Family", "price": 68000, "image": "https://i.imgur.com/qTkZJAz.jpg", "hp": 125, "cc": 1600},
-    {"name": "Perodua Myvi", "seats": 5, "type": "Compact", "price": 50000, "image": "https://i.imgur.com/Z5wr8VO.jpg", "hp": 95, "cc": 1300},
-    {"name": "Toyota Vios", "seats": 5, "type": "Sedan", "price": 85000, "image": "https://i.imgur.com/2rAyABu.jpg", "hp": 107, "cc": 1500},
-    {"name": "Honda BR-V", "seats": 7, "type": "Family", "price": 90000, "image": "https://i.imgur.com/OzEj20k.jpg", "hp": 120, "cc": 1500},
-    {"name": "Hyundai Tucson", "seats": 5, "type": "SUV", "price": 160000, "image": "https://i.imgur.com/m0LjBp1.jpg", "hp": 181, "cc": 2000},
-    {"name": "Kia Carnival", "seats": 8, "type": "Family", "price": 200000, "image": "https://i.imgur.com/WnYgeZU.jpg", "hp": 199, "cc": 2200},
-    {"name": "Mazda CX-5", "seats": 5, "type": "SUV", "price": 170000, "image": "https://i.imgur.com/fMECnH1.jpg", "hp": 191, "cc": 2500},
-    
-    {"name": "BMW 3 Series", "seats": 5, "type": "Luxury", "price": 280000, "image": "https://i.imgur.com/ERBaVPK.jpg", "hp": 255, "cc": 2000},
-    {"name": "Mercedes-Benz C200", "seats": 5, "type": "Luxury", "price": 290000, "image": "https://i.imgur.com/S8Wr37R.jpg", "hp": 204, "cc": 1500},
-    {"name": "Audi A4", "seats": 5, "type": "Luxury", "price": 270000, "image": "https://i.imgur.com/8Whbklr.jpg", "hp": 201, "cc": 2000},
-    {"name": "Volkswagen Passat", "seats": 5, "type": "Luxury", "price": 180000, "image": "https://i.imgur.com/YKLy3ZT.jpg", "hp": 220, "cc": 2000},
-    {"name": "Lexus RX350", "seats": 5, "type": "Luxury", "price": 400000, "image": "https://i.imgur.com/wukZTkT.jpg", "hp": 275, "cc": 3500},
-    {"name": "BMW X5", "seats": 7, "type": "Luxury", "price": 450000, "image": "https://i.imgur.com/ifldL9x.jpg", "hp": 335, "cc": 3000},
-    {"name": "Mercedes-Benz GLC", "seats": 5, "type": "Luxury", "price": 380000, "image": "https://i.imgur.com/WZ7FM8v.jpg", "hp": 258, "cc": 2000},
-    {"name": "Maybach S580", "seats": 5, "type": "Ultra Luxury", "price": 1600000, "image": "https://i.imgur.com/jU6qHCL.jpg", "hp": 496, "cc": 4000},
-    {"name": "Maserati Levante", "seats": 5, "type": "Luxury", "price": 700000, "image": "https://i.imgur.com/tUXLtGj.jpg", "hp": 345, "cc": 3000},
+    {"name": "Perodua Alza", "seats": 7, "type": "Family", "price": 70000, "hp": 105, "cc": 1500},
+    {"name": "Proton Exora", "seats": 7, "type": "Family", "price": 68000, "hp": 125, "cc": 1600},
+    {"name": "Perodua Myvi", "seats": 5, "type": "Compact", "price": 50000, "hp": 95, "cc": 1300},
+    {"name": "Toyota Vios", "seats": 5, "type": "Sedan", "price": 85000, "hp": 107, "cc": 1500},
+    {"name": "Honda BR-V", "seats": 7, "type": "Family", "price": 90000, "hp": 120, "cc": 1500},
+    {"name": "Hyundai Tucson", "seats": 5, "type": "SUV", "price": 160000, "hp": 181, "cc": 2000},
+    {"name": "Kia Carnival", "seats": 8, "type": "Family", "price": 200000, "hp": 199, "cc": 2200},
+    {"name": "Mazda CX-5", "seats": 5, "type": "SUV", "price": 170000, "hp": 191, "cc": 2500},
 
-    {"name": "Ferrari 488 GTB", "seats": 2, "type": "Supercar", "price": 1200000, "image": "https://i.imgur.com/YbG5kkz.jpg", "hp": 661, "cc": 3900},
-    {"name": "Lamborghini Huracan", "seats": 2, "type": "Supercar", "price": 1500000, "image": "https://i.imgur.com/QTjAXEq.jpg", "hp": 631, "cc": 5200},
-    {"name": "Porsche 911 GT3", "seats": 2, "type": "Supercar", "price": 1700000, "image": "https://i.imgur.com/KqJqKgD.jpg", "hp": 502, "cc": 4000},
-    {"name": "McLaren 720S", "seats": 2, "type": "Supercar", "price": 1600000, "image": "https://i.imgur.com/4j5ImbZ.jpg", "hp": 710, "cc": 4000},
-    {"name": "Aston Martin DB11", "seats": 4, "type": "Supercar", "price": 1300000, "image": "https://i.imgur.com/N6ZrLMx.jpg", "hp": 528, "cc": 4000}
+    {"name": "BMW 3 Series", "seats": 5, "type": "Luxury", "price": 280000, "hp": 255, "cc": 2000},
+    {"name": "Mercedes-Benz C200", "seats": 5, "type": "Luxury", "price": 290000, "hp": 204, "cc": 1500},
+    {"name": "Audi A4", "seats": 5, "type": "Luxury", "price": 270000, "hp": 201, "cc": 2000},
+    {"name": "Volkswagen Passat", "seats": 5, "type": "Luxury", "price": 180000, "hp": 220, "cc": 2000},
+    {"name": "Lexus RX350", "seats": 5, "type": "Luxury", "price": 400000, "hp": 275, "cc": 3500},
+    {"name": "BMW X5", "seats": 7, "type": "Luxury", "price": 450000, "hp": 335, "cc": 3000},
+    {"name": "Mercedes-Benz GLC", "seats": 5, "type": "Luxury", "price": 380000, "hp": 258, "cc": 2000},
+    {"name": "Maybach S580", "seats": 5, "type": "Ultra Luxury", "price": 1600000, "hp": 496, "cc": 4000},
+    {"name": "Maserati Levante", "seats": 5, "type": "Luxury", "price": 700000, "hp": 345, "cc": 3000},
+
+    {"name": "Ferrari 488 GTB", "seats": 2, "type": "Supercar", "price": 1200000, "hp": 661, "cc": 3900},
+    {"name": "Lamborghini Huracan", "seats": 2, "type": "Supercar", "price": 1500000, "hp": 631, "cc": 5200},
+    {"name": "Porsche 911 GT3", "seats": 2, "type": "Supercar", "price": 1700000, "hp": 502, "cc": 4000},
+    {"name": "McLaren 720S", "seats": 2, "type": "Supercar", "price": 1600000, "hp": 710, "cc": 4000},
+    {"name": "Aston Martin DB11", "seats": 4, "type": "Supercar", "price": 1300000, "hp": 528, "cc": 4000}
 ]
 
 # Monthly loan formula
@@ -66,7 +66,6 @@ def main():
                 car["seats"] >= seats and
                 (car_type == "Any" or car["type"] == car_type)
             ):
-                st.image(car["image"], width=300)
                 st.markdown(f"### {car['name']}")
                 st.write(f"Type: {car['type']}")
                 st.write(f"Seats: {car['seats']}")
